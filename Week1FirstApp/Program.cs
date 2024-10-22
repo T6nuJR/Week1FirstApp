5c8296a08f9769564eebad53efe3ea9f0c1f2b78
Tonu Riisimae <example@mail.com>
2024-10-22 11:59:12 +0300

    my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//This is a comment
+
+string myName = "Tonu";
+string myLastName = "Riisimae";
+
+//Console.WriteLine("Hello" 0 " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName} !");
+    
